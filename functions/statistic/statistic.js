1dc192af667dce6ed28f9622b8f3ea322b4f677f
ChrisZ32 <rays636@gmail.com>
2022-08-04 18:14:01 +0800

    [feature](Todo/新功能：統計) 新增統計查看功能(目前僅完成過濾器表單建置)

---
+++ functions/statistic/statistic.js
@@ -0,0 +1,153 @@
+// import characters from '../../data/characters';
+// import idb from 'https://cdn.jsdelivr.net/npm/idb@7/+esm';
+
+/**
+ * 欄位選項生成器
+ * @param {string} fieldId 下拉式選單欄位ID(CSS Selector形式)
+ */
+const selectOptionsGenerator = (fieldId) => {
+    let internalData = {}
+    switch (fieldId) {
+        case '#select_character':
+            internalData = characters
+            break;
+        case '#select_map':
+            internalData = maps
+            break;
+        default:
+            break;
+    }
+
+    let selectOptions = {}
+    Object.keys(internalData).forEach(typeOrName => {
+        if (typeof internalData[typeOrName] === "object") {
+            let type = typeOrName
+            selectOptions[type] = []
+            Object.keys(internalData[type]).forEach(name => {
+                selectOptions[type].push(
+                    (function () {
+                        let optionElement = document.createElement('option')
+                        optionElement.value = internalData[type][name]
+                        optionElement.text = name
+                        return optionElement
+                    })()
+                )
+            })
+        } else if (typeof internalData[typeOrName] === "string") {
+            let type = "no_type"
+            let name = typeOrName
+            if (!selectOptions[type]) {
+                selectOptions[type] = []
+            }
+            selectOptions[type].push(
+                (function () {
+                    let optionElement = document.createElement('option')
+                    optionElement.value = internalData[name]
+                    optionElement.text = name
+                    return optionElement
+                })()
+            )
+        }
+    })
+    let selectField = document.querySelector(fieldId)
+    if (selectField) {
+        Object.keys(selectOptions).forEach(type => {
+            if (type != "no_type") {
+                let optionGroupEle = document.createElement('optgroup')
+                optionGroupEle.label = type
+                selectOptions[type].forEach(oneOptions => {
+                    optionGroupEle.appendChild(oneOptions)
+                })
+                selectField.appendChild(optionGroupEle)
+            } else {
+                selectOptions["no_type"].forEach(oneOptions => {
+                    selectField.appendChild(oneOptions)
+                })
+            }
+        })
+    }
+}
+
+/**
+ * 表單資料驗證器
+ */
+const formDataValidators = {
+    character: function (userInputData) {
+        return (userInputData.character) ? true : false
+    },
+    map: function (userInputData) {
+        return (userInputData.map) ? true : false
+    },
+    gameResult: function (userInputData) {
+        return (userInputData.result) ? true : false
+    }
+}
+
+// 頁面載入時，為下拉式選單塞選項
+window.onload = () => {
+    let form = document.querySelector('#record')
+
+    selectOptionsGenerator('#select_character')
+    selectOptionsGenerator('#select_map')
+}
+
+// 表單Submit後的Event Handler
+document.querySelector('#record').addEventListener('submit', async (e) => {
+    /* ------------------------------ Functions ------------------------------ */
+    /**
+     * 取得表單資料
+     * @returns 
+     */
+    const retrieveFormData = async function () {
+        let formData = new FormData(document.querySelector("#record"))
+        let userInputData = {
+            character: "",
+            map: "",
+            startdate: "",
+            enddate: ""
+        }
+        for (let pair of formData.entries()) {
+            switch (pair[0]) {
+                case "選擇角色":
+                    userInputData.character = pair[1]
+                    break;
+                case "選擇地圖":
+                    userInputData.map = pair[1]
+                    break;
+                case "起始日期":
+                    let startdate = pair[1] ? new Date(pair[1]) : new Date()
+                    startdate.setHours(0)
+                    startdate.setMinutes(0)
+                    startdate.setSeconds(0)
+                    userInputData.startdate = startdate
+                    break;
+                case "結束日期":
+                    let enddate = pair[1] ? new Date(pair[1]) : new Date()
+                    enddate.setHours(23)
+                    enddate.setMinutes(59)
+                    enddate.setSeconds(59)
+                    userInputData.enddate = enddate
+                    break;
+                default:
+                    break;
+            }
+        }
+        console.log("userInputData", userInputData)
+        return userInputData
+    }
+
+    const queryData = async function(userInputData) {
+        
+    }
+
+    /* ------------------------------ Main ------------------------------ */
+    console.log('submit', e)
+    if (e.preventDefault) e.preventDefault()
+
+    retrieveFormData()
+        .then()
+        .catch((formDataError) => {
+            console.error("Form Data Error", formDataError)
+            return false
+        })
+})
